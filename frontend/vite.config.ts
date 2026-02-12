47bd650835f457203f3cd3e69c49bd82c850fe7e
Dusha01 <Ia12Kotik@yandex.ru>
2026-02-12 13:23:02 +1000

    fx vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
+        host: true,
+        port: 5173,
 		proxy: {
 			'/api': {
 				target: 'http://localhost:8080',
